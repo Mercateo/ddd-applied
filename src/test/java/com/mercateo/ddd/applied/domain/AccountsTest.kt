405daa884825e8db7d7694a2beba1ee99c78b459
Andreas Wuerl <andi@tryb.de>
2018-02-04 22:29:51 +0100

    added more tests

--- src/test/java/com/mercateo/ddd/applied/domain/AccountsTest.kt
+++ src/test/java/com/mercateo/ddd/applied/domain/AccountsTest.kt
@@ -2,6 +2,7 @@ package com.mercateo.ddd.applied.domain
 
 import com.mercateo.ddd.applied.EventHandler
 import com.mercateo.ddd.applied.ReadModel
+import com.nhaarman.mockito_kotlin.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,5 +34,38 @@ class AccountsTest {
         val account = result.get()
         assertThat(account.holder).isEqualTo(holder)
         assertThat(account.balance).isEqualTo(BigDecimal.ZERO)
+    }
+
+    @Test
+    fun shouldReturnFailureForUnknownId() {
+        val result = uut.byId(AccountId())
+
+        assertThat(result).isEmpty()
+        assertThat(result.left).isNotNull()
+    }
+
+    @Test
+    fun shouldReturnAccountById() {
+        val accountId = AccountId()
+        val account = Account(id = accountId, balance = BigDecimal.ZERO, holder = AccountHolder("foo"))
+        whenever(readModel.accountById(accountId)).thenReturn(account)
+
+        val result = uut.byId(accountId)
+
+        assertThat(result).isNotEmpty()
+        assertThat(result.get()).isSameAs(account)
+    }
+
+    @Test
+    fun shouldGetListOfAllAccounts() {
+        val accounts = listOf(
+                Account(id = AccountId(), balance = BigDecimal.ZERO, holder = AccountHolder("foo")),
+                Account(id = AccountId(), balance = BigDecimal.ZERO, holder = AccountHolder("bar"))
+        )
+        whenever(readModel.getAccounts()).thenReturn(accounts)
+
+        val result = uut.getAll()
+
+        assertThat(result).extracting("holder").extracting("name").containsExactlyInAnyOrder("foo", "bar")
     }
 }
